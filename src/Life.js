b81ec8cf520dc9c784736166e0fbd5374073f357
Olga Staskevits <olga.stashkevitsh@gmail.com>
2022-12-14 17:09:31 +0200

    first commit

---
+++ src/Life.js
@@ -0,0 +1,13 @@
+function LifeComponent(props){
+    console.log('LifeComponent');
+
+    return(
+        <div>
+            LifeComponent
+            <button onClick ={() => props.setCount(props.count + 1)}>+</button>
+
+        </div>
+    )
+}
+
+export default LifeComponent;
