06c152adce78e742965fb004a5d4ec0a7c8c8868
Olga Staskevits <olga.stashkevitsh@gmail.com>
2022-12-20 15:18:06 +0200

    homework searchBar

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,35 +1,50 @@
+import React, { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
 import Map from './Map';
 import TimeSelector from './TimeSelector';
 import './body.scss';
+import SearchBar from './SearchBar';
 
 
 function Body() {
+
+    const [showBar, setShowBar] = useState(false);
+
+    const handleCloseBar = () => setShowBar(false);
+    const handleShowBar = () => setShowBar(true);
+
     return (
-        <Tabs
-            defaultActiveKey="now"
-            id="justify-tab-example"
-            className="mb-3"
-            justify
-        >
-            <Tab eventKey="now" title="Now">
-                <TimeSelector id="now" />
-                <Map />
-            </Tab>
-            <Tab eventKey="Forcast" title="Forcast">
-                <TimeSelector id="forcast" />
-                <Map />
+        <>
+            <Button variant="primary" onClick={handleShowBar}>
+                Launch
+            </Button>
+            <SearchBar show={showBar} handleClose = {handleCloseBar} />
+            <Tabs
+                defaultActiveKey="now"
+                id="justify-tab-example"
+                className="mb-3"
+                justify
+            >
+                <Tab eventKey="now" title="Now">
+                    <TimeSelector id="now" />
+                    <Map />
+                </Tab>
+                <Tab eventKey="Forcast" title="Forcast">
+                    <TimeSelector id="forcast" />
+                    <Map />
 
 
-            </Tab>
-            {/* <Tab eventKey="longer-tab" title="Loooonger Tab">
+                </Tab>
+                {/* <Tab eventKey="longer-tab" title="Loooonger Tab">
                 <TimeSelector id="longer"/>
                 <Map />
 
 
             </Tab> */}
-        </Tabs>
+            </Tabs>
+        </>
     );
 }
 
